03fe239405849ba697c52b18f06885cb53805895
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2022-12-04 18:37:16 +0100

    _

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 14:27:58 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/12/01 15:00:33 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/12/04 10:57:54 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,23 @@
 # include "mlx/mlx.h"
 # include "libft/libft.h"
 # include "libft/ft_printf/ft_printf.h"
+# include <math.h>
+# include <stdio.h>
+
+typedef struct s_map
+{
+	char	*tex[3]; // N,E,S,W char *path_to_file.xmp
+	int		*color[1]; // F,C (int *)(unsigned char [4]){r, g, b, a})
+	char	**data;
+}t_map;
+
+typedef struct s_cub
+{
+	void		*mlx;
+	s_struct	*s_map;
+}t_cub;
+
+// returns hex decoded rgba values as int pointer
+int	*ft_itox_rgba(int r, int g, int b, int a);
 
 #endif
