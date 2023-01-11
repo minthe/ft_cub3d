2915606f0e99705e1ce6c3441ba19f3be005ce34
dimbrea <dragosimbrea@yahoo.it>
2023-01-11 20:03:27 +0100

    not 3d but going there

--- ray.c
+++ ray.c
@@ -6,7 +6,7 @@
 /*   By: dimbrea <dimbrea@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 10:13:11 by dimbrea           #+#    #+#             */
-/*   Updated: 2023/01/11 17:30:10 by dimbrea          ###   ########.fr       */
+/*   Updated: 2023/01/11 20:00:22 by dimbrea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ void	draw_ln(t_var *var)
 	double	pixel_y;
 	int		pixels;
 
-	delta_x = var->plr->end_x - var->plr->pos_x;
-	delta_y = var->plr->end_y - var->plr->pos_y;
+	delta_x = var->map->wall_ex  - var->map->wall_sx;
+	delta_y = var->map->wall_ey  - var->map->wall_sy;
 	pixels = sqrt((delta_x * delta_x) + (delta_y * delta_y));
 	delta_x /= pixels;
 	delta_y /= pixels;
-	pixel_x = var->plr->pos_x;
-	pixel_y = var->plr->pos_y;
+	pixel_x = var->map->wall_sx;
+	pixel_y = var->map->wall_sy;
 	while (pixels)
 	{
 		img_pix_put(var, pixel_x, pixel_y, 0xFFFFFF);
@@ -36,53 +36,55 @@ void	draw_ln(t_var *var)
 	}
 }
 
-void	ft_cast_rayz(t_var *var, double radians)
-{
-	double ray_pos;
-	double ray_angle;
-	double	distance;
-	double x;
-	double y;
-	double dx;
-	double dy;
-	double fov;
+// void	ft_cast_rayz(t_var *var, double radians)
+// {
+// 	double ray_pos;
+// 	double ray_angle;
+// 	double	distance;
+// 	double x;
+// 	double y;
+// 	double dx;
+// 	double dy;
+// 	double fov;
 
-	(void)radians;
-	fov = FOV;
-	ray_pos = 0.0;
-	while(ray_pos < 1.0)
-	{
-		ray_angle = fov * ray_pos - fov / 2 + var->plr->p_angle;
-		var->plr->radians = ray_angle * M_PI / 180;
-		dx = cos(var->plr->radians);
-		dy = sin(var->plr->radians);
-		x = var->plr->pos_x;
-		y = var->plr->pos_y;
-		distance = 0;
-		while(distance < 10000)
-		{
-			img_pix_put(var, x, y, 0xFFFFFE);
-			if(ft_is_wall(var,x,y) > 0)
-			{
-				double walz = 0;
-				var->map->wall_height = SCREEN_HEIGHT / (distance / 1.0);
+// 	(void)radians;
+// 	fov = FOV;
+// 	ray_pos = 0.0;
+// 	while(ray_pos < 1.0)
+// 	{
+// 		ray_angle = fov * ray_pos - fov / 2 + var->plr->p_angle;
+// 		var->plr->radians = ray_angle * M_PI / 180;
+// 		dx = cos(var->plr->radians);
+// 		dy = sin(var->plr->radians);
+// 		x = var->plr->pos_x;
+// 		y = var->plr->pos_y;
+// 		distance = 0;
+// 		while(distance < 10000)
+// 		{
+// 			img_pix_put(var, x, y, 0xFFFFFE);
+// 			// printf("HERE\n");
+// 			if(ft_is_wall(var,x,y) > 0)
+// 			{
+// 				// double walz = 0;
+// 				var->map->wall_height = SCREEN_HEIGHT / (distance / 1.0);
 				
-				img_pix_put(var, x, y, 0xFFFFFF);
-				while(walz < var->map->wall_height)
-				{
-					if (var->map->wall_height > 1)
-					walz++;
-				}
-				break;
-			}
-			x += dx;
-			y += dy;
-			distance +=1;
-		}
+// 				// img_pix_put(var, x, y, 0xFFFFFF);
+// 				// printf("oritgoeswronghere\n");
+// 				// while(walz < var->map->wall_height)
+// 				// {
+// 				// 	if (var->map->wall_height > 1)
+// 				// 	walz++;
+// 				// }
+// 				break;
+// 			}
+// 			x += dx;
+// 			y += dy;
+// 			distance +=1;
+// 		}
 		
-		ray_pos += 0.01;
-	}
-}
+// 		ray_pos += 0.05;// decrease for more rays
+// 	}
+// }
 // void	ft_cast_rayz(t_var *var, double radians)
 // {
 // 	double ray_pos;
@@ -110,26 +112,25 @@ void	ft_cast_rayz(t_var *var, double radians)
 // 		{
 // 			if(ft_is_wall(var,x,y) > 0)
 // 			{
-// 				// double walz = 0;
-// 				var->map->wall_sx = ((var->plr->pos_x + distance * cos(var->plr->radians)) * ray_pos - (SCREEN_WIDTH / 2));
-// 				var->map->wall_ex = (var->plr->pos_x + distance * cos(var->plr->radians)) * ray_pos + (SCREEN_WIDTH / 2);
-// 				var->map->wall_sy = ((var->plr->pos_y + distance * sin(var->plr->radians)) * ray_pos - (SCREEN_HEIGHT / 2));
-// 				var->map->wall_ey = (var->plr->pos_y + distance * sin(var->plr->radians)) * ray_pos + (SCREEN_HEIGHT / 2);
-// 				// printf("%d wall_sx\n", var->map->wall_sx);
-// 				// printf("%d wall_ex\n", var->map->wall_ex);
-// 				// printf("%d wall_sy\n", var->map->wall_sy);
-// 				// printf("%d wall_ey\n", var->map->wall_ey);
+// 				// int walz = 0;
+// 				var->map->wall_sx = ((var->plr->pos_x + distance * cos(var->plr->radians)) - (SCREEN_WIDTH / 2)) * -1;
+// 				var->map->wall_ex = (var->plr->pos_x + distance * cos(var->plr->radians)) + (SCREEN_WIDTH / 2);
+// 				var->map->wall_sy = ((var->plr->pos_y + distance * sin(var->plr->radians)) - (SCREEN_HEIGHT / 2)) * -1;
+// 				var->map->wall_ey = (var->plr->pos_y + distance * sin(var->plr->radians)) + (SCREEN_HEIGHT / 2);
+// 				printf("%d wall_start_x\n", var->map->wall_sx);
+// 				printf("%d wall_end_x\n", var->map->wall_ex);
+// 				printf("%d wall_start_y\n", var->map->wall_sydouble);
+// 				printf("%d wall_end_y\n", var->map->wall_ey);
 				
-// 				var->map->wall_height = SCREEN_HEIGHT / distance;
-// 				draw_ln(var);
-// 				// while(walz < var->map->wall_height)
+// 				// var->map->wall_height = SCREEN_HEIGHT / distance;
+// 				// draw_ln(var);
+// 				// while(walz < SCREEN_WIDTH)
 // 				// {
-// 					// img_pix_put(var, var->map->wall_sx, var->map->wall_sy, 0xFFFFFF);
-// 					// img_pix_put(var, var->map->wall_ex, var->map->wall_ey, 0xFFFFFF);
+// 					img_pix_put(var, var->map->wall_ex, var->map->wall_ey, 0xFFFFFF);
+// 					img_pix_put(var, var->map->wall_sx, var->map->wall_sy, 0xFFFFFF);
 // 					// img_pix_put(var, x, y, 0xFFFFFF);
 // 					// img_pix_put(var, x, y, 0xFFFFFF);
-// 				// 	if (var->map->wall_height > 1)
-// 				// 	walz++;
+// 					// walz++;
 // 				// }
 // 				break;
 // 			}
@@ -141,3 +142,113 @@ void	ft_cast_rayz(t_var *var, double radians)
 // 		ray_pos += 0.05;
 // 	}
 // }
+
+// void ft_draw wall
+// void ft_cast_rayz(t_var *var, double radians)
+// {
+// 	double ray_pos = 0.0;
+// 	double ray_angle;
+// 	double distance;
+// 	double x;
+// 	double y;
+// 	double dx;
+// 	double dy;
+// 	double fov;	
+// 	(void)radians;
+// 	fov = FOV;
+// 	while (ray_pos <= 1.0)
+// 	{
+// 		ray_angle = fov * ray_pos - fov / 2 + var->plr->p_angle;
+// 		var->plr->radians = ray_angle * M_PI / 180;
+// 		dx = cos(var->plr->radians);
+// 		dy = sin(var->plr->radians);
+// 		x = var->plr->pos_x;
+// 		y = var->plr->pos_y;
+// 		distance = 0;
+// 		while (distance < 10000) 
+// 		{
+// 			if (ft_is_wall(var, x, y) > 0)
+// 			{
+// 				var->map->wall_sx = (var->plr->pos_x + distance * cos(var->plr->radians));
+// 				var->map->wall_ex = (var->plr->pos_x + distance * cos(var->plr->radians)) + (SCREEN_WIDTH / 2);
+// 				var->map->wall_sy = (var->plr->pos_y + distance * sin(var->plr->radians));
+// 				var->map->wall_ey = (var->plr->pos_y + distance * sin(var->plr->radians)) + (SCREEN_HEIGHT / 2);	
+// 				//Calculate wall height
+// 				var->map->wall_height = SCREEN_HEIGHT / distance;
+// 				while(var->map->wall_sx < var->map->wall_ex + 1)
+// 				{
+// 					int yyy = var->map->wall_sy;
+// 					while(yyy < var->map->wall_ey + 1)
+// 					{
+// 						img_pix_put(var, var->map->wall_sx, yyy, 0xFFFFFF);
+// 						yyy++;
+// 					}
+// 					var->map->wall_sx++;
+// 				}
+// 				// printf("%d wall_start_x\n", var->map->wall_sx);
+// 				// printf("%d wall_end_x\n", var->map->wall_ex);
+// 				// printf("%d wall_start_y\n", var->map->wall_sy);
+// 				// printf("%d wall_end_y\n", var->map->wall_ey);
+// 				// ft_draw_wall(var, var->map->wall_sx, var->map->wall_ex, var->map->wall_sy, var->map->wall_ey, var->map->wall_height);
+// 				break;
+// 			}
+// 				x += dx;
+// 				y += dy;
+// 				distance += 1;
+// 		}
+// 		ray_pos += 0.1;
+// 	}
+// }
+
+void ft_cast_rayz(t_var *var, double radians)
+{
+	double ray_pos;
+	double ray_angle;
+	double distance;
+	double x;
+	double y;
+	double dx;
+	double dy;
+	double fov;
+	// double wall_height;
+	double wall_x;
+	double wall_top;
+	double wall_bottom;
+	double wall_length;
+	// double camera_x
+	(void)radians;
+	fov = 60;
+	ray_pos = 0.0;
+	while (ray_pos <= 1.0)
+	{
+		//Calculate the angle of the current ray
+		ray_angle = fov * ray_pos - (fov / 2) + var->plr->p_angle;
+		var->plr->radians = ray_angle * M_PI / 180;
+		dx = cos(var->plr->radians);
+		dy = sin(var->plr->radians);
+		x = var->plr->pos_x;
+		y = var->plr->pos_y;
+		distance = 0;
+		while (distance < 10000)
+		{
+			if (ft_is_wall(var, x, y) > 0)
+			{
+				wall_length = SCREEN_HEIGHT / distance;
+				wall_x = (x - var->plr->pos_x) / (y - var->plr->pos_y);
+				wall_x = (wall_x * SCREEN_WIDTH / 2) + (SCREEN_WIDTH / 2);
+				wall_top = (SCREEN_HEIGHT - wall_length) / 2;
+				wall_bottom = wall_top + wall_length;
+				while (wall_top < wall_bottom)
+				{
+					img_pix_put(var, wall_x, wall_top, 0xFFFFFF);
+					wall_top++;
+				}
+				break;
+			}
+			x += dx;
+			y += dy;
+			distance += 1;
+		}
+		ray_pos += 0.002;
+	}
+}
