545fec24ca03da2d81aac7e036cd7ef2aa609d8e
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2023-01-14 22:02:29 +0100

    map_validation wip

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 21:24:03 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2023/01/14 20:19:52 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2023/01/14 22:01:06 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,23 @@
 
 void	check_map(t_var *var, char **map, int map_lines, int p_set)
 {
-	(void)map;
+	int	col;
+	int	row;
 	(void)map_lines;
 	(void)p_set;
+
 	if (!var->data->p_set)
 		error_msg_exit("map error: no player found");
+	row = 0;
+	while(row < var->data->map_lines)
+	{
+		col = 0;
+		while(col < var->data->map_col)
+		{
+			if (map[row][col] == '0')
+				printf("0 at: %d,%d\n", row, col);
+			col++;
+		}
+		row++;
+	}
 }
